2a0d0a80f6584d735a817cf9adf0e828db41fbe8
AckimJnr <acklongwe@gmail.com>
2023-04-18 09:36:21 +0200

    Update checkout event

---
+++ src/BuildingBlocks/EventBus.Messages/Events/ShoppingCartItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventBus.Messages.Events
+{
+    public class ShoppingCartItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal SubTotal { get; set; }
+        public string Color { get; set; }
+    }
+}
+
+
